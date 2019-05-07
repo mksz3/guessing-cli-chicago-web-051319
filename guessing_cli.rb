384b393a5134c853ba854976d1891128c73f8835
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 19:59:21 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,10 +3,11 @@ def run_guessing_game
   guess = gets.downcase.chomp
   random = rand(1..6)
   num = random.to_s
+  
   while guess
     if guess == 'exit'
       puts "Goodbye!"
-      return
+      break
     elsif guess == num
       puts "You guessed the correct number!"
     else 
