ae8877f29bdff46884dacf7d94719c52faf17780
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 19:41:21 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,8 +4,9 @@ def run_guessing_game
   random = rand(1..6)
   
   while guess.downcase != 'exit'
-    if guess == random
+    if guess.to_i == random
       puts "You guessed the correct number!"
+      run_guessing_game
     else 
       puts "The computer guessed #{random}."
       run_guessing_game
