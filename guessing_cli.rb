63addfa180645eee5a11d6c7e0db780719b8e006
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 20:16:15 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,8 +6,10 @@ def run_guessing_game
     random = rand(1..6)
     if guess.to_i == random
       puts "You guessed the correct number!"
+      guess = gets.chomp
     else 
       puts "The computer guessed #{random}."
+      guess = gets.chomp
     end
   end
   puts "Goodbye!"
