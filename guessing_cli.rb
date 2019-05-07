5217841574b21221070b1c254fd7ec08f941d780
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 19:34:49 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,9 @@
 # Code your solution here!
 def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  userGuess = gets.chomp
+  random = 
   
+  while userGuess.downcase != 'exit'
+    
 end
