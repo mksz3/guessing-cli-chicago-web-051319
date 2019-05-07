e47408b10db619f70662e05b46bdfb86c2997029
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 19:54:25 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,17 +1,17 @@
 def run_guessing_game
-  run_guessing_game
   puts "Guess a number between 1 and 6."
   guess = gets.chomp
   random = rand(1..6)
   
-  # while guess
-  #   if guess.to_i == random
-  #     puts "You guessed the correct number!"
-  #   elsif guess.downcase == "exit"
-  #     break
-  #   else
-  #     puts "The computer guessed #{random}."
-  #   end
-  # end
+  while guess.downcase != 'exit'
+    if guess.to_i == random
+      puts "You guessed the correct number!"
+      run_guessing_game
+    else 
+      puts "The computer guessed #{random}."
+      run_guessing_game
+    break
+    end
+  end
     puts "Goodbye!"
 end
